57e35d60ed9d42dcfa74820065b0a6a980e97094
Hugo Parent <hugo.parent-leduc@epfl.ch>
2023-10-01 01:08:05 +0200

    Added table, VAT, Covered %

--- app.py
+++ app.py
@@ -6,12 +6,20 @@ app.secret_key = "some_random_secret_key"  # for session management
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
+        # Calculate VAT and covered amounts based on input percentages
+        amount = float(request.form['amount'])
+        vat_amount = float(request.form['vat_amount']) if 'vat_amount' in request.form else 0
+        coverage = float(request.form.get('coverage', 100)) / 100
+        covered_amount = amount * coverage
+
         # Add the new expense to the session
         expense = {
             'date': request.form['date'],
             'description': request.form['description'],
             'merchant': request.form['merchant'],
-            'amount': float(request.form['amount'])
+            'amount': amount,
+            'vat_amount': vat_amount,
+            'covered_amount': covered_amount
         }
         if 'expenses' not in session:
             session['expenses'] = []
@@ -20,6 +28,11 @@ def index():
 
     expenses = session.get('expenses', [])
     return render_template('index.html', expenses=expenses)
+
+@app.route('/clear', methods=['GET'])
+def clear():
+    session.clear()
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
