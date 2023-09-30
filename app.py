54fbf5e896513fba85799917adbf63e0efb1109b
Hugo Parent <hugo.parent-leduc@epfl.ch>
2023-10-01 00:50:55 +0200

    Added HTML

--- app.py
+++ app.py
@@ -1,6 +1,25 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template, request, session, redirect, url_for
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+app = Flask(__name__)
+app.secret_key = "some_random_secret_key"  # for session management
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        # Add the new expense to the session
+        expense = {
+            'date': request.form['date'],
+            'description': request.form['description'],
+            'merchant': request.form['merchant'],
+            'amount': float(request.form['amount'])
+        }
+        if 'expenses' not in session:
+            session['expenses'] = []
+        session['expenses'].append(expense)
+        return redirect(url_for('index'))
+
+    expenses = session.get('expenses', [])
+    return render_template('index.html', expenses=expenses)
+
+if __name__ == '__main__':
+    app.run(debug=True)
